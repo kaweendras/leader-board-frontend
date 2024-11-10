6bb44535fa4ecf02e815a3c642b2b739e80c9a23
kaweendras <salitha.wijerathna@gmail.com>
2024-11-10 22:24:00 +0530

    CHG | Geeting Base URL from env

--- src/api/apiClient.jsx
+++ src/api/apiClient.jsx
@@ -1,8 +1,15 @@
 // frontend/src/api/apiClient.js
 import axios from "axios";
 
+const apiUrl = import.meta.env.VITE_API_BASE_URL;
+
+if (!apiUrl) {
+  console.error("API URL is not set");
+  throw new Error("API URL is not set");
+}
+
 const apiClient = axios.create({
-  baseURL: "http://localhost:4000/api/v1/users", // Base URL for user endpoints
+  baseURL: apiUrl, // Base URL for user endpoints
   headers: {
     "Content-Type": "application/json",
     authorization: `Bearer ${sessionStorage.getItem("jwt") || ""}`,
